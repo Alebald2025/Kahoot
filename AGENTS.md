ce7a5a6630299889a20dd656c8f9a76292cd708d
Alejandro Baldeon <a24josbalesp@inspedralbes.cat>
2025-12-04 23:25:44 +0100

    Los botones de Kahoots se crean automaticamten en un ScrollView y se pueden escoger. Script que lee Json creado, script para llenar botones creado

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: KahootGit
+- Unity version: Unity 2022.3.45f1
+- Active game object:
+  - Name: Content
+  - Tag: Untagged
+  - Layer: UI
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/KahootSelector.cs
+++ Assets/Scripts/KahootSelector.cs
@@ -1,13 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class KahootSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform buttonContainer;
+    public GameObject buttonPrefab;
+    public TMP_InputField usernameInput;
+
+    private string persistentPath => Application.persistentDataPath + "/Kahoots/";
+
     void Start()
     {
-        
+        LoadKahootsFromResources();
+        LoadKahootsFromPersistentData();
+    }
+
+    void LoadKahootsFromPersistentData()
+    {
+        TextAsset[] jsonFiles = Resources.LoadAll<TextAsset>("KahootsDefault");
+        foreach (var file in jsonFiles)
+        {
+            KahootData kahoot = JsonUtility.FromJson<KahootData>(file.text);
+            CreateButton(kahoot.title, file.text);
+        }
+    }
+
+    void LoadKahootsFromResources()
+    {
+        if (!Directory.Exists(persistentPath)) Directory.CreateDirectory(persistentPath);
+        string[] files = Directory.GetFiles(persistentPath, "*.json");
+        foreach (string path in files)
+        {
+            string json = File.ReadAllText(path);
+            KahootData kahoot = JsonUtility.FromJson<KahootData>(json);
+            CreateButton(kahoot.title, json);
+        }
+    }
+
+    void CreateButton(string title, string jsonContent)
+    {
+        GameObject btn = Instantiate(buttonPrefab, buttonContainer);
+        btn.GetComponentInChildren<TMP_Text>().text = title;
+        btn.GetComponent<Button>().onClick.AddListener(() => OnKahootSelected(jsonContent));
+    }
+
+    void OnKahootSelected(string jsonContent)
+    {
+        string username = usernameInput.text;
+        PlayerPrefs.SetString("Username", username);
+        PlayerPrefs.SetString("SelectedKahoot", jsonContent);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
     }
 
     // Update is called once per frame
